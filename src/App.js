9826cc0f46281cd82a17f9002bcad4a9977bf10e
vijaycham <104141481+vijaycham@users.noreply.github.com>
2024-12-29 18:42:23 +0530

    CRA to Vite

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="text-3xl font-bold text-green-800">Hello world</div>;
-}
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+const App = () => {
+  return (
+    <div className='font-bold text-red-700 text-4xl'>Appp</div>
+  )
+}
+
+export default App
