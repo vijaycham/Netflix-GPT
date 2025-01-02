d2eb46cd70a23521bc8b6a65ef4508c6fe6c2f5c
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-01-02 22:35:12 +0530

    created redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from "react"
-import Body from "./components/Body"
-
-
+import React from "react";
+import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = () => {
-
-  
   return (
-    
-    <div><Body/></div>
-  )
-}
+    <div>
+      <Provider store={appStore}>
+        <Body />
+      </Provider>
+    </div>
+  );
+};
 
-export default App
+export default App;
