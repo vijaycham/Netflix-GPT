c23ccd7f5865f1c117d26feb938e2ed176d93a7e
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-01-06 21:24:34 +0530

    Made Video Title

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,28 +1,23 @@
-import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import {addNowPlayingMovies} from "../utils/movieSlice"
-
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 const Browse = () => {
+  useNowPlayingMovies();
 
-  const dispatch = useDispatch()
-  const getNowPlayingMovies = async () => {
-   const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results)) 
-  };
-
-   useEffect(() => {
-     getNowPlayingMovies();
-   }, []);
   return (
     <div>
       <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
+      {/*
+            MainContainer
+              - videoBackgorund
+              - VideoTitle
+            SecondaryContainer
+              - Movielist *n
+                - cards
+      */}
     </div>
   );
 };
---
+++ src/components/VideoTitle.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="pt-48 px-16">
+      <h1 className=" text-6xl font-bold"> {title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div className="">
+        <button className="bg-gray-500 text-black px-10 py-3 text-lg rounded-md mx-4 opacity-80">
+          ▶ Play
+        </button>
+        <button className="bg-gray-500 text-black px-10 py-3 text-lg rounded-md opacity-80">
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
