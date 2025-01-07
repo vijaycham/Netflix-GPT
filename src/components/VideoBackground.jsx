3b85ab28d2b97e1979d7eafdeb80567b04a0fca0
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-01-07 08:44:04 +0530

    Made VideoBackground for maincontainer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -7,11 +7,16 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?autoplay=1&mute=1&showinfo=0&rel=0&loop=1&playlist=" +
+          trailerVideo?.key +
+          "&modestbranding=1&iv_load_policy=3"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-48 px-16">
+    <div className="py-96 px-28 absolute text-white bg-gradient-to-r from-black w-screen aspect-video">
       <h1 className=" text-6xl font-bold"> {title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <p className="py-6 mt-3 text-lg w-1/4 h-32 overflow-y-scroll scrollbar-hide">
+        {overview}
+      </p>
       <div className="">
-        <button className="bg-gray-500 text-black px-10 py-3 text-lg rounded-md mr-4 opacity-80">
-          ▶ Play
+        <button className="bg-white text-black px-8 py-3 text-lg rounded-md mr-4 my-5 hover:bg-opacity-80">
+          ▶︎ Play
         </button>
-        <button className="bg-gray-500 text-black px-10 py-3 text-lg rounded-md opacity-80">
-          More Info
+        <button className="bg-gray-500 text-white px-6 py-3 text-lg rounded-md bg-opacity-50 hover:bg-opacity-25">
+          ⓘ More Info
         </button>
       </div>
     </div>
